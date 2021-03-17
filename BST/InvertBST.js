5f86abb46d5cfd539693360f5d65cdd058527d29
Will <whbladon@gmail.com>
2021-03-17 14:33:04 -0700

    invert BST

---
+++ BST/InvertBST.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return root
+    
+    //traverse
+    if (root.left) invertTree(root.left)
+    if (root.right) invertTree(root.right)
+    
+    //invert pointers
+    let temp = root.right
+    root.right = root.left
+    root.left = temp
+    
+    return root
+};
