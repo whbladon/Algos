b7586f840c050ed6a80e880180d0eb323c41826e
Will <whbladon@gmail.com>
2021-04-12 00:13:52 -0700

    Priority Queue

--- Heap/PriorityQueueNaive.js
+++ Heap/PriorityQueueNaive.js
@@ -1,6 +1,13 @@
 //Linked list implementation 
 //O(n) insert, O(1) remove
 
+class Node {
+  constructor(val, priority) {
+    this.value = val
+    this.priority = priority
+    this.next = null
+  }
+}
 
 class PriorityQueue {
     constructor() {
@@ -8,18 +15,25 @@ class PriorityQueue {
     }
     
     insert(value, priority) {
-      const newNode = new Node(value, priority);
-      if (!this.first || priority > this.first.priority) {
-        newNode.next = this.first;
-        this.first = newNode;
-      } else {
-        let pointer = this.first;
-        while (pointer.next && priority < pointer.next.priority) {
-          pointer = pointer.next;
-        }
-        newNode.next = pointer.next;
-        pointer.next = newNode;
+      const newNode = new Node(value, priority)
+      let currNode = this.first
+
+      //if no first node
+      if (!this.first) this.first = newNode
+      //if newNode has higher priority than first node
+      else if (priority > currNode.priority) {
+        this.first = newNode
+        newNode.next = currNode
+      } 
+
+      //Loop until we locate last node with higher priority
+      while (currNode.next && priority < currNode.next.priority){
+        currNode = currNode.next
       }
+      
+      //insert node 
+      newNode.next = currNode.next
+      currNode.next = newNode
     }
     
     remove() {
