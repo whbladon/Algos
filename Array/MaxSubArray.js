f58c0a6c4763ba8e257df56c836037dff2342d3f
Will <whbladon@gmail.com>
2021-04-11 20:58:20 -0700

    binary search

--- Array/MaxSubArray.js
+++ Array/MaxSubArray.js
@@ -4,21 +4,44 @@
  * @return {number}
  */
 
-//Kadane's algo
 
+
+
+/*
+You are given an array of integers with both positive and negative numbers.
+A valid subarray is any slice of consecutive elements from the array.
+
+exe:
+maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]);
+returns 18 from subarray [3, 10, -4, 7, 2]
+
+maxSubarray([15, 20, -5, 10]); 
+// returns 40 from subarray [15, 20, -5, 10]
+*/
 
  const maxSubArray = (array) => { 
     let maxSum = -Infinity;
     let currentSum = 0; 
   
     for (let i = 0; i < array.length; i++) { 
+
+        //Add current element to currentSum
         currentSum = currentSum + array[i]; 
         
-        if (maxSum < currentSum) 
+        //If currentSum is greater than max sum, re-assign maxSum
+        if (currentSum > maxSum) 
             maxSum = currentSum; 
   
+        //If currentSum drops below zero, reset currentSum
         if (currentSum < 0) 
             currentSum = 0; 
     } 
     return maxSum; 
 } 
+
+
+
+
+
+
+
---
+++ BinarySearch/SearchInRotatedSortedArray.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    if (nums == null || nums.length == 0) return -1
+    
+    let l = 0
+    let r = nums.length - 1
+    
+    while (l < r){
+        let m = Math.floor((l+r)/2)
+        
+        if (nums[m] > nums[r]) l = m+1
+        else r = m
+    }
+    
+    let midpoint = l
+    r = nums.length - 1
+    l = 0
+    
+    if (target >= nums[midpoint] && target <= nums[r]){
+        l = midpoint
+    } else r = midpoint
+    
+    while (l<=r){
+        let m = Math.floor((l+r)/2)
+        if (nums[m] === target) return m
+        
+        if (nums[m]> target) r = m-1
+        else l = m+1
+    }
+     
+    return -1
+    
+};
