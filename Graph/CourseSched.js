b0cd554efb2290f81d8e32049bb9d021ba2a8ca1
Will <whbladon@gmail.com>
2021-04-29 17:37:27 -0700

    topo sort BFS

--- Graph/CourseSched.js
+++ Graph/CourseSched.js
@@ -1,21 +1,50 @@
 
+//DFS with set caching optimization
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+ var canFinish = function(numCourses, prerequisites) {
+    
+    //construct adjacency list as array of empty []
+    const adjList = [...Array(numCourses)].map(a=>[])
+    
+    const visited = new Set()
+    const visiting = new Set()
+    
+    for (let [course, preReq]  of prerequisites){
+        adjList[preReq].push(course)
+    }
+    
+    //send off recurse for every vertex with outgoing edges
+    for (let vIndex = 0; vIndex < adjList.length; vIndex++){
+        if (!DFS(vIndex)) return false
+    }
+    
 
-
-
-
-
-
-
-
-
-
-
-
+    function DFS(vertex) {
+        
+        //if we have already visited, return true
+        if (visited.has(vertex)) return true
+        if (visiting.has(vertex)) return false
+        
+        //add vertex to visiting set
+        visiting.add(vertex)
+        
+        for (let edge of adjList[vertex]){
+            if (!DFS(edge)) return false  
+        }
+        visiting.delete(vertex)
+        visited.add(vertex)
+        return true
+    }
+    
+    return true
+};
 
 
 //Naive backtracking 
-
-
 /**
  * @param {number} numCourses
  * @param {number[][]} prerequisites
