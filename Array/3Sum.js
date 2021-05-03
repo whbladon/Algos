cdb4021de415762ecff247f2d2b1cbeeb9502fca
Will <whbladon@gmail.com>
2021-05-03 16:57:33 -0700

    3sum

--- Array/3Sum.js
+++ Array/3Sum.js
@@ -2,52 +2,41 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const threeSum = function (nums) {
-  const output = [];
 
-  nums = nums.sort((a, b) => a - b);
-
-  //make sure take 2 off nums.length because that is last triple possible
-  for (let l = 0; l < nums.length - 2; l++) {
-    //conditional passes if first iteration OR the prevoius and current ele arent equal
-    if (l === 0 || nums[l] > nums[l - 1]) {
-      let m = l + 1;
-      let r = nums.length - 1;
-
-      //loop until pointers touch
-
-      while (m < r) {
-        const left = nums[l];
-        const middle = nums[m];
-        const right = nums[r];
-
-        let sum = left + right + middle;
-
-        //if match found, enter push to output
-        if (sum === 0) {
-          output.push([left, middle, right]);
-        }
-
-        //if sum is too small
-        if (sum < 0) {
-          let temp = m;
-          m++;
-
-          //loop keeps incrementing is duplicated are found
-          while (nums[m] === nums[temp] && m < r) {
-            m++;
+//Two pointer approach
+const threeSum = function(nums) {
+  nums = nums.sort((a,b) => a-b )
+  const output = []
+  
+  //loop through the array
+  for (let i = 0; i < nums.length - 2; i++){
+      const base = nums[i]
+      if (base > 0) break
+      
+      const target = 0 - base
+      let l = i + 1
+      let r = nums.length - 1
+      
+      while (l<r){
+          const right = nums[r]
+          const left = nums[l]
+          const localSum = right + left
+          
+          //check output condition
+          if (localSum === target) output.push([base, left, right])
+          
+          
+          //move pointers
+          if (localSum < target) l++
+          else { 
+              //detect duplicate r values
+              while (right === nums[--r]) {}
           }
-        } else {
-          let temp = r;
-          r--;
-
-          while (nums[r] === nums[temp] && m < r) {
-            r--;
-          }
-        }
       }
-    }
+      
+      //detect duplicate left values
+      while (base === nums[i+1]) i++
   }
-
-  return output;
-};
+          
+  return output
+}
